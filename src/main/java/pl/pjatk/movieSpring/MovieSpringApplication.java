dba3ab35561ee72264e7bca297b3c8ddf72fd0fe
s20500-pj <s20500@pjwstk.edu.pl>
2021-05-08 16:26:55 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/movieSpring/MovieSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.movieSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSpringApplication.class, args);
+	}
+
+}
