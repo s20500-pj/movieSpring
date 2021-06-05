af3d8b5e3e138530bf94ad4f1ae2d977a718d3df
s20500-pj <s20500@pjwstk.edu.pl>
2021-06-05 13:50:34 +0200

    added @Transactional

--- src/main/java/movie/jaz/demo/repository/MovieRepository.java
+++ src/main/java/movie/jaz/demo/repository/MovieRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+    @Transactional
     @Modifying
     @Query("update Movie m set m.isavailable = 1 where m.id = ?1")
     void changeAvailable(Long movieId);
